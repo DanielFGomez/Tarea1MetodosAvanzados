e2c9e5ea8025a5dc94a412e8a28a4472d648a566
DanielGomez <df.gomez280@uniandes.edu.co>
2016-02-29 00:22:16 -0500

    version final

--- Problema3Cuerpos/tresCuerpos.c
+++ Problema3Cuerpos/tresCuerpos.c
@@ -4,59 +4,105 @@
 #include <time.h>
 
 double *initArray(int n_puntos);
-void printArray(double * array1, double * array2, double * array3, double * array4, int n_puntos);
-void copy(double *origen, double *destino, int n_puntos);
-void *resetArray(double *array,int n_puntos);
 
-double fun(double x);
 double dp1(double q1);
 double dp3(double q1,double q3);
 void solve1(double *q1, double *p1, double dt, double Nsteps);
 void solve3(double *q1, double *q3, double *p3, double dt, double Nsteps);
-void print3(double *q1, double *q3, double *p3, int nPuntos);
+void solve1RK(double *q1, double *p1, double dt, double Nsteps);
+void solve3RK(double *q1, double *q3, double *p1, double *p3, double dt, double Nsteps);
+void print3(double *p1, double *q3, double *p3, int nPuntos, FILE *f);
+void printE(double dt, double *q1,double *p1, double Nsteps, FILE *f);
 double epsilon;
 
 
 
 int main(){
-	double *q1,*q3,*p1,*p3;
-	double dt=0.006;
-	//paper son 466667
-	int nSteps=500000;
-	int nInitCond=83;
-	srand48(time(NULL));
-	
-	epsilon = 1;
-	q1=initArray(nSteps);
-	q3=initArray(nSteps);
-	p1=initArray(nSteps);
-	p3=initArray(nSteps);
+  double *q1,*q3,*p1,*p3,*q1RK,*p1RK,*q3RK,*p3RK,*q1A,*q3A,*p1A,*p3A;
+  double dt=0.006;
+  int nSteps=466667;
+  int nInitCond=83;
+  //Inicializa el RNG
+  srand48(time(NULL));
 
-	//Tomado del paper
-	q1[0]=0.35355339059;
-	solve1(q1,p1,dt,nSteps);
-	
+  //Inicializar Arrays
+  epsilon =0.6;//Este valor dio los mapas que se visualizan mejor
+  q1=initArray(nSteps);
+  q3=initArray(nSteps);
+  p1=initArray(nSteps);
+  p3=initArray(nSteps);
 
-	int i;
-	for (i=0;i<nInitCond;i++){
-	  resetArray(q3,nSteps);
-	  resetArray(p3,nSteps);
-	  q3[0]=drand48()*1;
-	  solve3(q1,q3,p3,dt,nSteps);
-	  print3(p1,q3,p3,nSteps);
-	}
-	
-	return 0;
+  q1RK=initArray(nSteps);
+  p1RK=initArray(nSteps);
+  q3RK=initArray(nSteps);
+  p3RK=initArray(nSteps);
+
+  q1A=initArray(nSteps);
+  p1A=initArray(nSteps);
+  q3A=initArray(nSteps);
+  p3A=initArray(nSteps);
+  
+  //Resolver el movimiento de las masas grandes
+  q1[0]=0.35355339;
+  solve1(q1,p1,dt,nSteps);
+
+  q1RK[0]=q1[0];
+  solve1RK(q1RK,p1RK,dt,nSteps);
+
+  q1A[0]=0.4325;
+  solve1(q1A,p1A,dt,nSteps);
+  
+  //Crea los archivos donde se guardara la informacion  
+  FILE *fileSim = fopen("tresCuerpos.dat", "w");
+  FILE *fileRK = fopen("tresCuerposRK.dat", "w");
+  FILE *fileA = fopen("tresCuerposA.dat", "w");
+  
+  FILE *fileE = fopen("energia.dat", "w");
+  FILE *fileERK = fopen("energiaRK.dat", "w");
+
+  //Guarda los resultados de la energia para cada metodo de integracion
+  printE(dt,q1,p1,nSteps,fileE);
+  printE(dt,q1RK,p1RK,nSteps,fileERK);
+	 
+  int i;
+  for (i=0;i<nInitCond;i++){
+
+    //Pone condiciones iniciales aleatorias
+    q3[0]=drand48()*6-3;
+    p3[0]=drand48()*2-1;
+    q3RK[0]=q3[0];
+    p3RK[0]=p3[0];
+    q3A[0]=drand48()*2-1;
+    p3A[0]=drand48()-0.5;
+    
+    solve3(q1,q3,p3,dt,nSteps);
+    solve3RK(q1RK,q3RK,p1RK,p3RK,dt,nSteps);
+    solve3(q1A,q3A,p3A,dt,nSteps);
+    
+    print3(p1,q3,p3,nSteps,fileSim);
+    print3(p1RK,q3RK,p3RK,nSteps,fileRK);
+    print3(p1A,q3A,p3A,nSteps,fileA);
+  }
+    
+  fclose(fileSim);
+  fclose(fileRK);
+  fclose(fileA);  
+  fclose(fileE);
+  fclose(fileERK);
+  return 0;
 }
 
+//Derivada de p1
 double dp1(double  q1){
   return -2*q1/pow(4*q1*q1+epsilon*epsilon,3/2);
 }
 
+//Derivada de p3
 double dp3(double q1,double q3){
   return (q1-q3)/pow((q1-q3)*(q1-q3)+epsilon*epsilon/4,3/2) - (q1+q3)/pow((q1+q3)*(q1+q3)+epsilon*epsilon/4,3/2); 
 }  
 
+//Integrador simplectico
 void solve1(double *q1, double *p1, double dt, double Nsteps){
   int i;
   double a0=-pow(2,1/3)/(2-pow(2,1/3));
@@ -77,7 +123,7 @@ void solve1(double *q1, double *p1, double dt, double Nsteps){
     
       }
 }
-
+//Integrador simplectico
 void solve3(double *q1, double *q3, double *p3, double dt, double Nsteps){
   int i;
   double a0=-pow(2,1/3)/(2-pow(2,1/3));
@@ -98,72 +144,82 @@ void solve3(double *q1, double *q3, double *p3, double dt, double Nsteps){
   }
   
 }
-/*
-void solve3RK(double *q1, double *q3, double *p3, double dt, double Nsteps){
+//Integrador Runge Kutta
+void solve1RK(double *q1, double *p1, double dt, double Nsteps){
   double k1, k2, k3, k4;
   double l1, l2, l3, l4;
-  double x_in;
-  double v_in;
-  x_in = *x;
-  v_in = *v;
+  
   int i;
-  for (i=0;i<Nsteps-1
-  k1 = deriv_x(t,x_in,v_in);  
-  l1 = deriv_v(t,x_in,v_in);  
+  for (i=0;i<Nsteps-1;i++){
+    k1 = p1[i];  
+    l1 = dp1(q1[i]);  
 
-  k2 = deriv_x(t + delta_t*0.5, x_in + k1*delta_t*0.5, v_in + l1*delta_t*0.5);
-  l2 = deriv_v(t + delta_t*0.5, x_in + k1*delta_t*0.5, v_in + l1*delta_t*0.5) ;
+    k2 = p1[i] + 0.5*l1*dt;
+    l2 = dp1(q1[i] + 0.5*p1[i]*dt);
 
-  k3 = deriv_x(t + delta_t*0.5, x_in + k2*delta_t*0.5, v_in + l2*delta_t*0.5);
-  l3 = deriv_v(t + delta_t*0.5, x_in + k2*delta_t*0.5, v_in + l2*delta_t*0.5);
+    k3 = p1[i] + 0.5*l2*dt;
+    l3 = dp1(q1[i] + 0.5*p1[i]*dt);
 
-  k4 = deriv_x(t + delta_t, x_in + k3*delta_t, v_in + l3*delta_t);
-  l4 = deriv_v(t + delta_t, x_in + k3*delta_t, v_in + l3*delta_t);
+    k4 = p1[i] + l3*dt;
+    l4 = dp1(q1[i] + p1[i]*dt);
 
-  x_in += (k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0)*delta_t;
-  v_in += (l1/6.0 + l2/3.0 + l3/3.0 + l4/6.0)*delta_t;
+    q1[i+1] = q1[i] +  (k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0)*dt;
+    p1[i+1] = p1[i] +  (l1/6.0 + l2/3.0 + l3/3.0 + l4/6.0)*dt;
+  }
 }
-*/
+//Integrador Runge Kutta
+void solve3RK(double *q1, double *q3, double *p1, double *p3, double dt, double Nsteps){
+  double k1, k2, k3, k4;
+  double l1, l2, l3, l4;
+  
+  int i;
+  for (i=0;i<Nsteps-1;i++){
+    k1 = p3[i];  
+    l1 = dp3(q1[i],q3[i]);  
 
-void copy(double *origen, double *destino, int n_puntos){
-	int i;
-	for(i=0;i<n_puntos;i++){
-	destino[i] = origen[i];
-	}
-}
+    k2 = p3[i] + 0.5*l1*dt;
+    l2 = dp3(q1[i] + 0.5*p1[i]*dt, q3[i] + 0.5*k1*dt);
 
-void printArray(double * array1, double * array2, double * array3, double * array4, int n_puntos){
-	int i;
-	for(i=0;i<n_puntos;i++){
-	  printf("%f %f %f %f\n", array1[i],array2[i],array3[i],array4[i]);
-	}
+    k3 = p3[i] + 0.5*l2*dt;
+    l3 = dp3(q1[i] + 0.5*p1[i]*dt, q3[i] + 0.5*k2*dt);
+
+    k4 = p3[i] + l3*dt;
+    l4 = dp3(q1[i] + p1[i]*dt, q3[i] + k3*dt);
+
+    q3[i+1] = q3[i] +  (k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0)*dt;
+    p3[i+1] = p3[i] +  (l1/6.0 + l2/3.0 + l3/3.0 + l4/6.0)*dt;
+  }
 }
 
 double *initArray(int n_puntos){
-	double *array;
-	int i;
-	if(!(array = malloc(n_puntos * sizeof(double)))){
-	printf("Problema en initArray\n");
-	exit(1);
-	}
-	for(i=0;i<n_puntos;i++){
-	array[i] = 0.0;
-	}
-return array;
+  double *array;
+  int i;
+  if(!(array = malloc(n_puntos * sizeof(double)))){
+    printf("Problema en initArray\n");
+    exit(1);
+  }
+  for(i=0;i<n_puntos;i++){
+    array[i] = 0.0;
+  }
+  return array;
 }
 
-void *resetArray(double *array,int n_puntos){
-	int i;
-	for(i=0;i<n_puntos;i++){
-	array[i] = 0.0;
-	}
-}
 
- void print3(double *p1, double *q3, double *p3, int nPuntos){
+void print3(double *p1, double *q3, double *p3, int nPuntos, FILE *f){
    int i;
    for (i=0;i<nPuntos-1;i++){
      if(p1[i]*p1[i+1]<=0.0){
-       printf("%f %f\n",q3[i],p3[i]); //Asumo que si pasa de positivo a negativo o viceversa es pq paso por el 0.
+       fprintf(f,"%f %f\n",q3[i],p3[i]);
      }
    }
  }
+
+
+void printE(double dt, double *q1, double *p1, double Nsteps, FILE *f){
+  int i;
+  double E;
+  for (i=0;i<Nsteps;i++){
+    E=0.5*p1[i]*p1[i]-1.0/(2*pow(4*q1[i]*q1[i]+epsilon*epsilon,0.5));
+    fprintf(f,"%f %f\n",i*dt,E);
+  }
+}
